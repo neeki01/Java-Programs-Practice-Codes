ae06e4662ff10fb0994b587466f3267ca62ab636
neeki01 <kumarineeki321@gmail.com>
2024-07-23 00:37:02 +0530

    Assignments

---
+++ Assignment116.java
@@ -0,0 +1,15 @@
+//WAP with assert keyword
+package assignementBatch42;
+
+public class Assignment116 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		assert a>50:"Number used is not correct";
+		int sum=a+b;
+		System.out.println(sum);
+	}
+
+}
